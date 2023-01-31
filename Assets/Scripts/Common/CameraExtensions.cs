bcf80f96660b08c17e6247a1f2db4ea0c8bc944f
Maciej Rokicki <rokicki.mk@gmail.com>
2023-01-31 11:41:50 +0100

    calculating marker position

---
+++ Assets/Scripts/Common/CameraExtensions.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public static class CameraExtensions
+{
+    public static bool IsPointInFrustum(this Camera camera, Vector3 worldPoint)
+    {
+        Vector3 screenPos = camera.WorldToScreenPoint(worldPoint);
+
+        return screenPos.z >= camera.nearClipPlane
+               && screenPos.z <= camera.farClipPlane
+               && screenPos.x >= 0
+               && screenPos.x <= Screen.width
+               && screenPos.y >= 0
+               && screenPos.y <= Screen.height;
+    }
+}
---
+++ Assets/Scripts/Game/Enemy/EnemySpawner/EnemySpawnerPathMarker.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class EnemySpawnerPathMarker : MonoBehaviour
+{
+    private Vector3 basePosition;
+    private bool visable;
+
+    private void Start()
+    {
+        basePosition = transform.position;
+    }
+
+    private readonly Plane[] planes = new Plane[6];
+
+    private void Update()
+    {
+        visable = Camera.main.IsPointInFrustum(basePosition);
+
+        if (!visable)
+        {
+            Vector3 origin = Camera.main.transform.position;
+            Vector3 direction = (basePosition - Camera.main.transform.position).normalized;
+
+            Ray ray = new Ray(origin, direction);
+
+            float currentMinDistance = float.MaxValue;
+            Vector3 hitPoint = Vector3.zero;
+
+            GeometryUtility.CalculateFrustumPlanes(Camera.main, planes);
+
+            for (var i = 0; i < 4; i++)
+            {
+                if (planes[i].Raycast(ray, out var distance))
+                {
+                    if (distance < currentMinDistance)
+                    {
+                        hitPoint = ray.GetPoint(distance);
+                        currentMinDistance = distance;
+                    }
+                }
+            }
+
+            transform.position = hitPoint;
+        }
+        else
+        {
+            transform.position = basePosition;
+        }
+    }
+}
